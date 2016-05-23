65132137158244dc2c08dafd25173f7154821065
Ben Marshall <bm12656@my.bristol.ac.uk>
2016-05-23 14:08:51 +0100

    Add skeletons for AST files and properly link C library to it's test exe.

---
+++ src/verilog_ast.c
@@ -0,0 +1,7 @@
+/*!
+@file verilog_ast.c
+@brief Contains definitions of functions which 
+       operate on the Verilog Abstract Syntax Tree (AST)
+*/
+
+#include "verilog_ast.h"
---
+++ src/verilog_ast.h
@@ -0,0 +1,10 @@
+/*!
+@file verilog_ast.h
+@brief Contains Declarations of datastructures and functions which represent
+       and operate on the Verilog Abstract Syntax Tree (AST)
+*/
+
+#ifndef VERILOG_AST_H
+#define VERILOG_AST_H
+
+#endif
