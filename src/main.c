d1a467438e03200735f80757a37c068b121d5610
Ben Marshall <bm12656@my.bristol.ac.uk>
2016-06-12 17:20:32 +0100

    Allow test app to recieve as many file inputs as possible.  On branch master  Your branch is up-to-date with 'github/master'.
     Changes to be committed:
    	modified:   ../src/main.c

--- src/main.c
+++ src/main.c
@@ -9,33 +9,38 @@
 
 int main(int argc, char ** argv)
 {
-    if(argc != 2)
+    if(argc < 2)
     {
-        printf("ERROR. Please supply exactly one file path argument.\n");
+        printf("ERROR. Please supply at least one file path argument.\n");
         return 1;
     }
     else
     {
-        // Load the file.
-        FILE * fh = fopen(argv[1], "r");
-        
-        // Instance the parser.
-        verilog_parser parser = verilog_file_parse(fh);
+        int F = 0;
 
-        // Parse the file and store the result.
-        int result = verilog_parse_buffer(parser);
+        for(F = 1; F < argc; F++)
+        {
+            // Load the file.
+            FILE * fh = fopen(argv[F], "r");
+            
+            // Instance the parser.
+            verilog_parser parser = verilog_file_parse(fh);
 
-        fclose(fh);
-        
-        if(result == 0)
-        {
-            printf("Parse successful\n");
-            return 0;
-        }
-        else
-        {
-            printf("Parse failed\n");
-            return 1;
+            // Parse the file and store the result.
+            int result = verilog_parse_buffer(parser);
+
+            verilog_free_parser(parser);
+            fclose(fh);
+            
+            if(result == 0)
+            {
+                printf("Parse successful for %s\n",argv[F]);
+            }
+            else
+            {
+                printf("Parse failed for %s\n",argv[F]);
+                return 1;
+            }
         }
     }
     return 0;
