3f8658155ce178d3037e4227ebc07d5065074297
Ben Marshall <bm12656@my.bristol.ac.uk>
2016-06-13 17:53:07 +0100

    Added UDP tests to suite and stopped coverage tests exiting early.  On branch master  Your branch is ahead of 'github/master' by 3 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   bin/download-tests.sh
    	modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -39,7 +39,6 @@ int main(int argc, char ** argv)
             else
             {
                 printf("Parse failed for %s\n",argv[F]);
-                return 1;
             }
         }
     }
