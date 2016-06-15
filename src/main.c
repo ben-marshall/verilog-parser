1ee6c2c3fea091cf61d21e190dcf195b2149bc01
Ben Marshall <bm12656@my.bristol.ac.uk>
2016-06-15 18:44:17 +0100

    Fix incorrect reporting of successful parsing.  On branch master  Your branch is ahead of 'github/master' by 3 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -44,6 +44,7 @@ int main(int argc, char ** argv)
             else
             {
                 printf(" - Parse failed for\n");
+                if(argc<=2) return 1;
             }
         }
     }
