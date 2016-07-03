adfb78e2d687703dcda9df21aad63e566e4947a2
Ben Marshall <bm12656@my.bristol.ac.uk>
2016-07-03 15:47:00 +0100

    More tests.  On branch master  Your branch is up-to-date with 'github/master'.
     Changes to be committed:
    	new file:   tests/forever-disable.v
    	new file:   tests/reg-data-types.v

---
+++ tests/reg-data-types.v
@@ -0,0 +1,14 @@
+
+module reg_data_types();
+
+reg scal;
+reg [7:0] vect;
+reg [7:0] mem [31:0];
+integer i;
+integer i_mem [7:0];
+time t;
+time t_mem [3:0];
+real r;
+realtime rt1, rt2;
+
+endmodule
