d3eb9118a5734be3c360b0fc5b712d237f136c82
Ben Marshall <bm12656@my.bristol.ac.uk>
2016-07-05 21:14:24 +0100

    Started implementing pre-processor
    - Added Macro for token emission
    - Added files for preprocessor functions & structures.
     On branch feature/preprocessor
     Changes to be committed:
    	modified:   build/Makefile
    	new file:   src/verilog_preprocessor.c
    	new file:   src/verilog_preprocessor.h
    	modified:   src/verilog_scanner.l

---
+++ src/verilog_preprocessor.c
@@ -0,0 +1,6 @@
+/*!
+@file verilog_preprocessor.c
+@brief Contains function implementations to support source code preprocessing.
+*/
+
+#include "verilog_preprocessor.h"
---
+++ src/verilog_preprocessor.h
@@ -0,0 +1,30 @@
+/*!
+@file verilog_preprocessor.h
+@brief Contains function and data structures to support source code
+       preprocessing.
+*/
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "verilog_ast_common.h"
+
+#ifndef VERILOG_PREPROCESSOR_H
+#define VERILOG_PREPROCESSOR_H
+
+/*
+@brief Stores all of the contextual information used by the pre-processor.
+@details Stores things like:
+- Macro names and evaluations.
+- Default net types.
+- In Cell Defines.
+- IF/ELSE pre-processor directives.
+- Timescale directives
+*/
+typedef struct verilog_preprocessor_context_t{
+
+} verilog_preprocessor_context;
+
+#endif
+
