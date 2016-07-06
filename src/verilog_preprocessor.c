89452d78c2bd178cf0c5998073e3f98d4474b322
Ben Marshall <bm12656@my.bristol.ac.uk>
2016-07-06 10:34:02 +0100

    Added support for the default net-type directive.  On branch feature/preprocessor  Changes to be committed: 	modified:   src/verilog_preprocessor.c 	modified:   src/verilog_preprocessor.h 	modified:   src/verilog_scanner.l 	new file:   tests/net_type_directive.v

--- src/verilog_preprocessor.c
+++ src/verilog_preprocessor.c
@@ -14,6 +14,8 @@ verilog_preprocessor_context * verilog_new_preprocessor_context()
     tr -> in_cell_define = AST_FALSE;
     tr -> emit           = AST_TRUE;
 
+    tr -> net_types      = ast_list_new();
+
     return tr;
 }
 
@@ -31,4 +33,39 @@ void verilog_preproc_enter_cell_define()
 void verilog_preproc_exit_cell_define()
 {
     yy_preproc -> in_cell_define = AST_FALSE;
+}
+
+//! Creates and returns a new default net type directive.
+verilog_default_net_type * verilog_new_default_net_type(
+    unsigned int token_number,  //!< Token number of the directive.
+    unsigned int line_number,   //!< Line number of the directive.
+    ast_net_type type           //!< The net type.
+){
+    verilog_default_net_type * tr = 
+        ast_calloc(1,sizeof(verilog_default_net_type));
+
+    tr -> token_number = token_number;
+    tr -> line_number  = line_number;
+    tr -> type         = type;
+
+    return tr;
+}
+
+/*!
+@brief Registers a new default net type directive.
+@details Adds a record of the directive to the end of the linked list
+"net_types" in the global yy_preproc.
+*/
+void verilog_preproc_default_net(
+    unsigned int token_number,  //!< Token number of the directive.
+    unsigned int line_number,   //!< Line number of the directive.
+    ast_net_type type           //!< The net type.
+){
+    verilog_default_net_type * directive = verilog_new_default_net_type(
+        token_number,
+        line_number,
+        type
+    );
+
+    ast_list_append(yy_preproc -> net_types, directive);
 }
--- src/verilog_preprocessor.h
+++ src/verilog_preprocessor.h
@@ -14,6 +14,29 @@
 #ifndef VERILOG_PREPROCESSOR_H
 #define VERILOG_PREPROCESSOR_H
 
+
+// ----------------------- Default Net Type Directives ------------------
+
+/*!
+@brief Keeps track of the points at which default net type directives are
+encountered.
+*/
+typedef struct verilog_default_net_type_t{
+    unsigned int token_number;  //!< Token number of the directive.
+    unsigned int line_number;   //!< Line number of the directive.
+    ast_net_type type;          //!< The net type.
+} verilog_default_net_type;
+
+//! Creates and returns a new default net type directive.
+verilog_default_net_type * verilog_new_default_net_type(
+    unsigned int token_number,  //!< Token number of the directive.
+    unsigned int line_number,   //!< Line number of the directive.
+    ast_net_type type           //!< The net type.
+);
+
+
+// ----------------------- Preprocessor Context -------------------------
+
 /*
 @brief Stores all of the contextual information used by the pre-processor.
 @details Stores things like:
@@ -24,18 +47,23 @@
 - Timescale directives
 */
 typedef struct verilog_preprocessor_context_t{
-    ast_boolean  emit;           //!< Only emit tokens iff true.
-    unsigned int token_count;    //!< Keeps count of tokens processed.
-    ast_boolean  in_cell_define; //!< TRUE iff we are in a cell define.
+    ast_boolean     emit;           //!< Only emit tokens iff true.
+    unsigned int    token_count;    //!< Keeps count of tokens processed.
+    ast_boolean     in_cell_define; //!< TRUE iff we are in a cell define.
+    
+    ast_list      * net_types;      //!< Storage for default nettype directives
 } verilog_preprocessor_context;
+
 
 //! Stores all information needed for the preprocessor.
 extern verilog_preprocessor_context * yy_preproc;
+
 
 /*!
 @brief Creates a new pre-processor context.
 */
 verilog_preprocessor_context * verilog_new_preprocessor_context();
+
 
 /*!
 @brief Frees a preprocessor context and all child constructs.
@@ -44,16 +72,27 @@ void verilog_free_preprocessor_context(
     verilog_preprocessor_context * tofree
 );
 
+
 /*!
 @brief Tells the preprocessor we are now defining PLI modules and to tag
        them as such.
 */
 void verilog_preproc_enter_cell_define();
 
+
 /*!
 @brief Tells the preprocessor we are no longer defining PLI modules.
 */
 void verilog_preproc_exit_cell_define();
+
+/*!
+@brief Registers a new default net type directive.
+*/
+void verilog_preproc_default_net(
+    unsigned int token_number,  //!< Token number of the directive.
+    unsigned int line_number,   //!< Line number of the directive.
+    ast_net_type type           //!< The net type.
+);
 
 #endif
 
