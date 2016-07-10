a79709de29755b8ec211b3ef73943d3862a459e0
Ben Marshall <bm12656@my.bristol.ac.uk>
2016-07-10 20:04:15 +0100

    Add tests for more compliler directives

---
+++ tests/cd-missed.v
@@ -0,0 +1,15 @@
+
+// Missed compiler directives.
+
+`line 1 "a-new-file.v" 0
+
+`default_nettype wire
+`default_nettype tri
+`default_nettype tri0
+`default_nettype wand
+`default_nettype triand
+`default_nettype wor
+`default_nettype trior
+`default_nettype trireg
+
+`resetall
