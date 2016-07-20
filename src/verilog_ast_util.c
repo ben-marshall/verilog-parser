cda446445e4abf4e52e1dda4bcb7f9bf7d741a9d
Ben Marshall <bm12656@my.bristol.ac.uk>
2016-07-20 19:55:21 +0100

    Extra safety check  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   ../../src/verilog_ast_util.c

--- src/verilog_ast_util.c
+++ src/verilog_ast_util.c
@@ -110,7 +110,12 @@ ast_list * verilog_module_get_children(
     ast_module_declaration * module
 ){
     ast_list * tr = ast_list_new();
-    
+   
+    if(module == NULL)
+    {
+        return tr;
+    }
+
     int m;
     for(m = 0; m < module -> module_instantiations -> items; m++)
     {
